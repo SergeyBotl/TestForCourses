3e6addeaa496b5924b6c4290d1314a97a278562e
Sergey <bot74@mail.ru>
2017-01-02 01:08:56 +0200

    add

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -10,16 +10,14 @@ public class Main {
             triangle = new int[massive.length + 1];
             triangle[triangle.length - 1] = 0;
             for (int k = 0; k < massive.length; k++) {
-                triangle[k] = massive[k] + massive[massive.length - 1-k ];
+                triangle[k] = massive[k] + massive[massive.length - 1 - k];
             }
-
-            massive = new int[triangle.length];
             massive = triangle;
         }
         print(triangle);
     }
 
-    static void print(int[] ints) {
+    private static void print(int[] ints) {
         for (int i : ints) {
             if (i != 0) {
                 System.out.print(i + " ");
